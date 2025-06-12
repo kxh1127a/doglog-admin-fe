1ca78c3f992847f04ae3ac8b26e08a66f308deb0
kxh1127a <kxh1127a@gmail.com>
2025-06-12 11:54:50 +0900

    feat: 관리자 펫 조회 페이지 레이아웃 작업

--- src/pages/admin/pet/index.tsx
+++ src/pages/admin/pet/index.tsx
@@ -1,10 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {MdFace} from "react-icons/md";
-import {MdOutlineFaceRetouchingNatural} from "react-icons/md";
-import {MdOutlineFaceRetouchingOff} from "react-icons/md";
-import styles from '@/styles/Member.module.scss';
-import {TiArrowSortedDown} from "react-icons/ti";
-import {TiArrowSortedUp} from "react-icons/ti";
+import styles from '@/styles/Pet.module.scss';
 import {FaUserCircle} from "react-icons/fa";
 import {FaUserSlash} from "react-icons/fa";
 import {MdAdminPanelSettings} from "react-icons/md";
@@ -17,6 +12,7 @@ const Index = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [page, setPage] = useState(0);
     const [totalPages, setTotalPages] = useState(1);
+    const [countResults, setCountResults] = useState(0);
     // member type sort
     const [filter, setFilter] = useState('all');
     // member date sort
@@ -37,9 +33,6 @@ const Index = () => {
 
     const fetchUsers = async (page: number, size: number, sortBy: string, direction: string, filter: string) => {
         const url = `http://localhost:8089/member/api?page=${page}&size=${size}&sortBy=${sortBy}&direction=${direction}&filter=${filter}`;
-        // if (filter && filter != 'all') {
-        //     url += `&filter=${filter}`;
-        // }
         console.log(url);
         const res = await fetch(url);
         const data = await res.json();
@@ -56,28 +49,20 @@ const Index = () => {
         return data;
     }
 
-    // 검색 엔터
-    // const handleSearch = () => {
-    //     if (searchWord.trim() !== "") {
-    //         searchUsers(searchOption, searchWord, sortBy, sortDirection, filter).then((data) => {
-    //             setUsers(data.data.content);
-    //             setTotalPages(data.data.totalPages);
-    //         });
-    //     }
-    // };
-
     useEffect(() => {
         const handler = setTimeout(() => {
             if (searchWord.trim() === "") {
                 fetchUsers(page, 10, sortBy, sortDirection, filter).then(data => {
                     setUsers(data.data.content);
                     setTotalPages(data.data.totalPages);
+                    setCountResults(data.data.totalElements);
                 });
             } else {
                 searchUsers(page, 10, searchOption, searchWord, sortBy, sortDirection, filter).then(data => {
                     console.log(searchWord);
                     setUsers(data.data.content);
                     setTotalPages(data.data.totalPages);
+                    setCountResults(data.data.totalElements);
                 });
             }
         }, 500);
@@ -95,91 +80,50 @@ const Index = () => {
                 <section className={styles.upper_section}>
 
                     {/*sorting button*/}
-                    <div className={styles.user_status_btn_container}>
-                        <button className={filter == "all" ? styles.active_status : ''} onClick={() => {
-                            setFilter("all");
-                            setPage(0);
-                        }}><MdFace/> 전체회원
-                        </button>
-                        <button className={filter == "normal" ? styles.active_status : ''} onClick={() => {
-                            setFilter("normal");
-                            setPage(0);
-                        }}><MdOutlineFaceRetouchingNatural/> 활동회원
-                        </button>
-                        <button className={filter == "withdrawn" ? styles.active_status : ''} onClick={() => {
-                            setFilter("withdrawn");
-                            setPage(0);
-                        }}><MdOutlineFaceRetouchingOff/> 탈퇴회원
-                        </button>
-                    </div>
+                    {/*<div className={styles.user_status_btn_container}>*/}
+                    {/*    <button className={filter == "all" ? styles.active_status : ''} onClick={() => {*/}
+                    {/*        setFilter("all");*/}
+                    {/*        setPage(0);*/}
+                    {/*    }}><MdFace/> 전체회원*/}
+                    {/*    </button>*/}
+                    {/*    <button className={filter == "normal" ? styles.active_status : ''} onClick={() => {*/}
+                    {/*        setFilter("normal");*/}
+                    {/*        setPage(0);*/}
+                    {/*    }}><MdOutlineFaceRetouchingNatural/> 활동회원*/}
+                    {/*    </button>*/}
+                    {/*    <button className={filter == "withdrawn" ? styles.active_status : ''} onClick={() => {*/}
+                    {/*        setFilter("withdrawn");*/}
+                    {/*        setPage(0);*/}
+                    {/*    }}><MdOutlineFaceRetouchingOff/> 탈퇴회원*/}
+                    {/*    </button>*/}
+                    {/*</div>*/}
                     <div className={styles.search_container}>
                         <div className={styles.select_box}>
                             <select onChange={(e) => setSearchOption(e.target.value)}>
-                                <option value="name">이름</option>
-                                <option value="userName">아이디</option>
-                                <option value="email">이메일</option>
-                                <option value="phone">핸드폰번호</option>
+                                <option value="">이름</option>
+                                <option value="">품종</option>
                             </select>
                         </div>
 
-                        <input type="text" placeholder="검색어를 입력하세요."
+                        <input type="text" placeholder="반려견 정보를 입력하세요."
                                onChange={(e) => {
                                    setSearchWord(e.target.value);
                                    setPage(0);
                                }}
-                            // onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
                         />
+                        <div className={styles.resultBox}> 총 <strong>{countResults}</strong>개의 데이터가 조회되었습니다. </div>
                     </div>
 
                 </section>
                 <section className={styles.lower_section}>
                     <div className={styles.user_table}>
                         <div className={`${styles.user_header} ${styles.header}`}>
-                            <div>유저기본정보</div>
-                            <div>전화번호</div>
-                            <div>반려견정보</div>
-                            <div>가입일
-                                {
-                                    isDescendingCreatedAt
-                                        ? <TiArrowSortedDown
-                                            className={`${sortBy == "createdAt" ? styles.active_sort_btn : ''}`}
-                                            onClick={() => {
-                                                setIsDescendingCreatedAt(false);
-                                                setSortBy("createdAt");
-                                                // setPage(0);
-                                                setSortDirection("asc");
-                                            }}/>
-                                        : <TiArrowSortedUp
-                                            className={`${sortBy == "createdAt" ? styles.active_sort_btn : ''}`}
-                                            onClick={() => {
-                                                setIsDescendingCreatedAt(true);
-                                                setSortBy("createdAt");
-                                                // setPage(0);
-                                                setSortDirection("desc");
-                                            }}/>
-                                }
-                            </div>
-                            <div>마지막접속
-                                {
-                                    isDescendingLastLoginAt
-                                        ? <TiArrowSortedDown
-                                            className={`${sortBy == "lastLoginAt" ? styles.active_sort_btn : ''}`}
-                                            onClick={() => {
-                                                setIsDescendingLastLoginAt(false);
-                                                setSortBy("lastLoginAt");
-                                                // setPage(0);
-                                                setSortDirection("desc");
-                                            }}/>
-                                        : <TiArrowSortedUp
-                                            className={`${sortBy == "lastLoginAt" ? styles.active_sort_btn : ''}`}
-                                            onClick={() => {
-                                                setIsDescendingLastLoginAt(true);
-                                                setSortBy("lastLoginAt");
-                                                // setPage(0);
-                                                setSortDirection("asc");
-                                            }}/>
-                                }
-                            </div>
+                            <div>반려견 이름</div>
+                            <div>생년월일</div>
+                            <div>품종</div>
+                            <div>몸무게</div>
+                            <div>성별</div>
+                            <div>동물등록번호</div>
                         </div>
 
                         <div className={styles.user_body}>
@@ -189,25 +133,19 @@ const Index = () => {
                                          className={`${styles.user_row} ${!user.isEnabled ? styles.user_row_disabled : ''}`}
                                          onClick={() => {handleClick(user.id)}}>
                                         <div className={styles.user_info}>
-                                            {/*<img className={`${styles.profile_icon}`}*/}
-                                            {/*     style={{backgroundImage: `url(http://localhost:8089/${user.petProfileImageUrl})`}}/>*/}
-                                            {
-                                                !user.isEnabled ? <FaUserSlash className={styles.user_icon}/> :
-                                                    (
-                                                        user.memberRole === 'ADMIN' ?
-                                                            <MdAdminPanelSettings className={styles.admin_icon}/> :
-                                                            <FaUserCircle className={styles.user_icon}/>
-                                                    )
-                                            }
+                                            <img className={`${styles.profile_icon}`}
+                                                 style={{backgroundImage: `url(http://localhost:8089/${user.petProfileImageUrl})`}}/>
+
                                             <div>
-                                                <div><strong>{user.name} / {user.userName}</strong></div>
-                                                <div className={styles.email}>{user.email}</div>
+                                                <div><strong>{user.name}</strong></div>
+                                                <div className={styles.email}>{user.userName}</div>
                                             </div>
                                         </div>
                                         <div>{user.phone}</div>
                                         <div>{user.petName} ({user.petBirthDate})</div>
                                         <div>{user.createdAt}</div>
                                         <div>{user.lastLoginInfo}</div>
+                                        <div>{user.phone}</div>
                                     </div>
                                 ))
                             }
